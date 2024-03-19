4639e8306804562afe508d5c3d7f8ddbad6c7dbf
s0Armaan <157718193+s0Armaan@users.noreply.github.com>
2024-03-19 09:01:17 +0100

    Update test.js

--- views/test.js
+++ views/test.js
@@ -1,16 +1,17 @@
 const fetch = require('node-fetch');
 
-const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTY0MjBmM2IwNDFlMDkxMjgyZmVjZGRlMjU2NTAzZiIsInN1YiI6IjY1YjBlNmI5ZWEzN2UwMDE5M2U0NjI5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bg_SH3Y0udfbjbl0qYoxzAEj2z0_35g26sXN6mjxZnQ'
-  }
-};
+function fetchPopularMovies() {
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer YOUR_TMDB_API_KEY'
+    }
+  };
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-  
+  return fetch(url, options)
+    .then(res => res.json())
+    .then(json => json.results)
+    .catch(err => console.error('error:' + err));
+}
